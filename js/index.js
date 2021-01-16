a2a62a8f8e7c3acfece82397dd7ddb82d336feaa
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-01-15 21:00:15 -0500

    add focus and focusout event listeners

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const header = document.querySelector('.main-navigation');
+const navGroup = document.querySelectorAll('.nav-link');
 const imgGroup = document.querySelectorAll('img');
 const btnGroup = document.querySelectorAll('.btn');
 function fadeIn(event) {
@@ -14,6 +15,15 @@ function updateHeader(event) {
     };
     
 };
+function highlightLink(event) {
+    if (event.type === 'focus') {
+        this.style.color = "#17A2B8";
+        this.style.fontWeight = "bold";
+    } else {
+        this.style.color = "#000";
+        this.style.fontWeight = "normal";
+    }
+};
 function filterImg(event) {
     if (event.type === 'mouseover') {
         event.target.style.filter = "grayscale(100%)";
@@ -25,7 +35,6 @@ function resizeBtn(event) {
     if (event.type === 'mousedown') {
         this.style.transform = "scale(1.2)";
     } else if (event.type === 'transitionend' && event.propertyName === 'transform') {
-        console.log(event)
         this.style.transform = "scale(1)";
     }
 };
@@ -33,13 +42,17 @@ Object.assign(document.body.style, {opacity: '0', transition: 'all 1.5s linear'}
 Object.assign(header.style, {zIndex: '999', transition: 'all .2s linear'});
 window.addEventListener('load', fadeIn);
 document.addEventListener('scroll', updateHeader);
+navGroup.forEach((link) => {
+    link.addEventListener('focus', highlightLink);
+    link.addEventListener('focusout', highlightLink);
+});
 imgGroup.forEach((img) => {
     img.style.transition = "all .2s linear";
     img.addEventListener('mouseover', filterImg);
     img.addEventListener('mouseleave', filterImg);
 });
 btnGroup.forEach((btn) => {
-    btn.style.transition = "all .5s linear";
+    btn.style.transition = "all .3s linear";
     btn.addEventListener('mousedown', resizeBtn);
     btn.addEventListener('transitionend', resizeBtn);
 });
