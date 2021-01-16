93286e11f6206013aa3ddafefde9250246ced2c6
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-01-15 22:00:10 -0500

    stop prop and prevent default

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const navGroup = document.querySelectorAll('.nav-link');
 const imgGroup = document.querySelectorAll('img');
 const btnGroup = document.querySelectorAll('.btn');
 const sectionGroup = document.querySelectorAll('section');
+const destinationGroup = document.querySelectorAll('.destination');
 function fadeIn(event) {
     document.body.style.opacity = "1";
 };
@@ -14,11 +15,17 @@ function updateHeader(event) {
         header.style.background = "#17A2B8";
         header.style.height = "60px";
     };
-    
+    navGroup.forEach((link) => {
+        link.style.color = '#000';
+        link.style.fontWeight = 'normal';
+    });
 };
 function highlightLink(event) {
-    if (event.type === 'focus') {
+    if (event.type === 'focus' && window.scrollY === 0) {
         this.style.color = "#17A2B8";
+        this.style.fontWeight = "bold";
+    } else if (event.type === 'focus') {
+        this.style.color = "#fff";
         this.style.fontWeight = "bold";
     } else {
         this.style.color = "#000";
@@ -38,6 +45,7 @@ function resizeBtn(event) {
     } else if (event.type === 'transitionend' && event.propertyName === 'transform') {
         this.style.transform = "scale(1)";
     }
+    event.stopPropagation();
 };
 function highlightSection(event) {
     if (event.type === 'mouseenter') {
@@ -46,6 +54,9 @@ function highlightSection(event) {
         this.querySelector('h2').style.backgroundColor = "#fff";
     }
 };
+function highlightDestination(event) {
+    this.style.border = '10px dotted #FFEBCD';
+};
 Object.assign(document.body.style, {opacity: '0', transition: 'all 1.5s linear'});
 Object.assign(header.style, {zIndex: '999', transition: 'all .2s linear'});
 window.addEventListener('load', fadeIn);
@@ -53,6 +64,7 @@ document.addEventListener('scroll', updateHeader);
 navGroup.forEach((link) => {
     link.addEventListener('focus', highlightLink);
     link.addEventListener('blur', highlightLink);
+    link.addEventListener('click', (event) => event.preventDefault());
 });
 imgGroup.forEach((img) => {
     img.style.transition = "all .2s linear";
@@ -65,7 +77,11 @@ btnGroup.forEach((btn) => {
     btn.addEventListener('transitionend', resizeBtn);
 });
 sectionGroup.forEach((section) => {
+    if (section.classList.contains('content-pick')) return;
     section.querySelector('h2').style.transition = "all .3s linear";
     section.addEventListener('mouseenter', highlightSection);
     section.addEventListener('mouseleave', highlightSection);
+});
+destinationGroup.forEach((destination) => {
+    destination.addEventListener('mousedown', highlightDestination);
 });
